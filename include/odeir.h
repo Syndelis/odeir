2d9e6ec3030fc8e944cc1870b9c3b401ebe01157
cbindgen-action <actions@github.com>
2023-04-13 19:54:00 +0000

    Update generated C/C++ Bindings

--- include/odeir.h
+++ include/odeir.h
@@ -9,6 +9,16 @@
 #include "opaque_rust_types.hpp"
 
 
+struct String;
+
+struct MetaData {
+    double start_time;
+    double end_time;
+    double delta_time;
+};
+
+using NodeId = uint32_t;
+
 template<typename T>
 struct BoxedSlice {
     const T *ptr;
@@ -19,9 +29,11 @@ struct BoxedSlice {
             std::printf("Failed to free BoxedSlice %p\n", this);
         }
     }
+
     BoxedSlice(): ptr(nullptr), len(0), destructor(nullptr) {}
+
     BoxedSlice(std::vector<T> vector): len(vector.size()) {
-        T* mem = new T[vector.size()];
+        T* mem = new T[len];
         std::copy(vector.begin(), vector.end(), mem);
         this->destructor = [](const BoxedSlice* slice) {
             delete[] slice->ptr;
@@ -29,9 +41,11 @@ struct BoxedSlice {
         };
         this->ptr = mem;
     }
+
     size_t length() const {
         return len;
     }
+
     const T& operator[](size_t idx) const {
         if (idx < len) {
             return ptr[idx];
@@ -41,9 +55,63 @@ struct BoxedSlice {
     }
 };
 
+struct Node {
+    enum class Tag {
+        Population,
+        Combinator,
+    };
+
+    struct Population_Body {
+        NodeId id;
+        String name;
+        String related_constant_name;
+    };
+
+    struct Combinator_Body {
+        NodeId id;
+        String name;
+        uint32_t operation;
+        BoxedSlice<NodeId> inputs;
+    };
+
+    Tag tag;
+    union {
+        Population_Body population;
+        Combinator_Body combinator;
+    };
+
+    ~Node() {
+        switch (tag) {
+            case Tag::Population: population.~Population_Body(); break;
+            case Tag::Combinator: combinator.~Combinator_Body(); break;
+
+        }
+    }
+
+    Node(const Node& other)
+     : tag(other.tag) {
+        switch (tag) {
+            case Tag::Population: ::new (&population) (Population_Body)(other.population); break;
+            case Tag::Combinator: ::new (&combinator) (Combinator_Body)(other.combinator); break;
+
+        }
+    }
+};
+
+struct Constant {
+    String name;
+    double value;
+};
+
+struct Model {
+    MetaData meta_data;
+    BoxedSlice<Node> nodes;
+    BoxedSlice<Constant> constants;
+};
+
 
 extern "C" {
 
-BoxedSlice _tmp();
+Model model_from_cstr(const char *json_cstr);
 
 } // extern "C"

ccb36518c82335f3d5447506eac259cffc6b775f
Brenno Lemos <brenno@syndel.is>
2023-09-06 17:23:40 -0300

    refactor: prefer iteratorbase separated into its own file

--- include/odeir_reader.hpp
+++ include/odeir_reader.hpp
@@ -13,49 +13,7 @@
 #include <iostream>
 #include <vector>
 
-template <typename Derived, typename SourceType, typename ReturnType>
-class IterableBase {
-public:
-    IterableBase(const std::vector<SourceType>& data) : data_(data) {}
-
-    class Iterator {
-    public:
-        explicit Iterator(const IterableBase* iterable, size_t index) : iterable_(iterable), index_(index) {}
-
-        ReturnType operator*() {
-            return iterable_->dereference(index_);
-        }
-
-        Iterator& operator++() {
-            ++index_;
-            return *this;
-        }
-
-        bool operator!=(const Iterator& other) const {
-            return index_ != other.index_;
-        }
-
-    private:
-        const IterableBase* iterable_;
-        size_t index_;
-    };
-
-    Iterator begin() const {
-        return Iterator(static_cast<const Derived*>(this), 0);
-    }
-
-    Iterator end() const {
-        return Iterator(static_cast<const Derived*>(this), data_.size());
-    }
-
-private:
-    virtual ReturnType dereference(size_t index) const = 0;
-
-protected:
-    std::vector<SourceType> data_;
-};
-
-// -----------------------------------------------------------------------------
+#include <odeir_iterator.hpp>
 
 class NodeIterator : public IterableBase<NodeIterator, internal_api::NodeId, internal_api::Node*> {
 public:

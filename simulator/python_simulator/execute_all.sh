6481a641af0a161b480c9c83667dee59ec953343
Diego Augusto S. C <diego.augusto@protonmail.com>
2023-10-11 18:00:44 -0300

    Temp2

--- simulator/python_simulator/execute_all.sh
+++ simulator/python_simulator/execute_all.sh
@@ -1,8 +1,4 @@
-python3 simulate.py \
-	--ode-params 0.1 0.1 0.1 0.1 0.1 \
-	--y0 100 100 5 0 0 0 \
-	--tf 50 \
-	--dt 0.1 \
-	| python3 plot.py \
-		--output-file ".local/inhibitive_competition.png"
-
+#!/usr/bin/env sh
+cd $(dirname $(realpath $0))
+"python3" "simulate.py" "--dt" "0.1" "--tf" "10" "--output" "output.csv" "--y0" "100" "50" "--ode-params" "1.1" "0.9" "1" "0.8"
+"python3" "plot.py" "--input-file" "output.csv" "--output-file" "output.png" "--components" "x" "y"
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -22,17 +22,23 @@ struct Args {
     no_run: bool,
 }
 
+fn real_path(name: impl AsRef<Path>) -> PathBuf {
+    name.as_ref().canonicalize().unwrap()
+}
+
 fn run_simulation(folder: &Path, model: &Model) -> std::io::Result<()> {
     let delta_time = model.metadata.delta_time.to_string();
-    let mut ode_constants: Vec<_> = model.equations
+    let mut ode_constants: Vec<_> = model
+        .equations
         .get_constants()
         .filter_map(|c| match c {
             Argument::Value { value, .. } => Some(value),
-            _ => None
+            _ => None,
         })
         .map(ToString::to_string)
         .collect();
-    let (mut ode_initial, components): (Vec<_>, Vec<_>) = model.equations
+    let (mut ode_initial, components): (Vec<_>, Vec<_>) = model
+        .equations
         .get_populations()
         .filter_map(|pop| match pop {
             Argument::Value { value, name } => Some((value.to_string(), name)),
@@ -44,28 +50,39 @@ fn run_simulation(folder: &Path, model: &Model) -> std::io::Result<()> {
     ode_constants.insert(0, "--ode-params".into());
 
     std::env::set_current_dir(folder)?;
-    std::process::Command::new("python3")
+    let mut simulator = std::process::Command::new("python3");
+    simulator
         .arg("simulate.py")
         .args(&["--dt", &delta_time])
         .args(&["--tf", &model.metadata.end_time.to_string()])
         .args(&["--output", "output.csv"])
         .args(&ode_initial)
-        .args(&ode_constants)
-    .spawn()
-    .unwrap()
-    .wait()
-    .unwrap();
+        .args(&ode_constants);
 
-    std::process::Command::new("python3")
+    let mut plotter = std::process::Command::new("python3");
+    plotter
         .arg("plot.py")
-        .args(&["--input-file",  "output.csv"])
-        .args(&["--output-file",  "output.png"])
+        .args(&["--input-file", "output.csv"])
+        .args(&["--output-file", "output.png"])
         .arg("--components")
-        .args(components)
-    .spawn()
-    .unwrap()
-    .wait()
-    .unwrap();
+        .args(components);
+
+    // Save execute_all.sh
+    {
+        let prelude = "#!/usr/bin/env sh\ncd $(realpath $0)".to_string();
+        let mut commands = vec![prelude];
+        commands.extend_from_slice(
+            [&simulator, &plotter]
+                .map(|i| format!("{:?}", i))
+                .as_slice(),
+        );
+
+        std::fs::write("execute_all.sh", commands.join("\n"))?;
+    }
+
+    for mut p in [simulator, plotter] {
+        p.spawn().unwrap().wait().unwrap();
+    }
 
     Ok(())
 }

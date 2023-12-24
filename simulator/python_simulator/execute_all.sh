fac900e6c4c635039ac0a0707f48947204b8d9bf
Brenno Lemos <dev@brenno.codes>
2023-12-23 23:35:16 -0300

    chore: remove simulator python code

--- simulator/python_simulator/execute_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-cd $(dirname $(realpath $0))
-"python3" "simulate.py" "--dt" "0.1" "--tf" "10" "--output" "output.csv" "--y0" "100" "50" "--ode-params" "1.1"
-"python3" "plot.py" "--input-file" "output.csv" "--output-file" "output.png" "--components" "x" "y"

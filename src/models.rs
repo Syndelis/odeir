ba1ac5839e309561c1737edbed6edfec447426da
alexbprr <alexbprr@gmail.com>
2024-04-29 10:42:03 -0300

    remocao da estrutura das edos do odeir

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,3 @@
-use std::collections::BTreeMap;
 
 use serde::{Deserialize, Serialize};
 
@@ -70,67 +69,5 @@ pub struct Equation {
     pub operates_on: Option<String>,
     pub argument: String,
     pub contribution: char,
-}
-
-//ODE structure for the real time Rust solver 
-#[derive(Debug, Clone)]
-pub struct OdeEquation {
-    name: String,
-    text: String, //texto da equação a ser avaliado
-    expressions: Vec<String>,
-    value: f64,
-}
-
-impl OdeEquation {
-    pub fn new(name: String, text: String) -> Self {
-        Self {
-            name: name,
-            text: text,
-            expressions: vec![],
-            value: 0.0,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct OdeSystem {
-    pub equations: BTreeMap<String,OdeEquation>,
-    pub values: BTreeMap<String,f64>, 
-}
-//gera os índices das populações em ordem alfabética.
-
-impl OdeSystem {
-    pub fn new() -> Self{
-        Self {
-            equations: BTreeMap::new(),
-            values: BTreeMap::new(),
-        }
-    }
-
-    pub fn create_ode_system(input: String) -> OdeSystem{
-        let mut ode_system = OdeSystem::new();
-
-        let lines = input.trim().split("\n").collect::<Vec<_>>();
-    
-        for line in lines {   
-
-            let new_line = line.split("=")
-                        .filter(|&s| !s.is_empty())
-                        .collect::<Vec<_>>();
-            let population = new_line[0].trim();
-            let equation = new_line[1].trim();
-            
-            let expressions = equation.split("+")
-                        .filter(|&s| !s.is_empty())
-                        .collect::<Vec<_>>();                    
-    
-            let mut ode = OdeEquation::new(population.to_string(), equation.to_string());
-            ode.expressions = expressions.iter().map(|&s| s.to_string()).collect::<Vec<String>>();
-
-            ode_system.equations.insert(population.to_string(), ode);
-        }
-
-        return ode_system
-    }
 }
 
